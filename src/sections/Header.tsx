20b1ab4a6bcb2187f6809e95f421839efd71c4bc
prisca onyeagoro <priscaonyeagoro@gmail.com>
2024-09-11 14:13:49 +0100

    header.tsx

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -7,7 +7,7 @@ export const Header = () => {
     <header>
       <div className="bg-black text-white text-sm flex justify-center items-center py-3">
         <div className="inline-flex gap-1 items-center">
-          <p>Get started for free right now</p>
+          <p>Get started for free</p>
           <ArrowRight className="h-4 w-4 inline-flex justify-center items-center" />
         </div>
       </div>
