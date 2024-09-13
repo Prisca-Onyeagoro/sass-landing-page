922c9cc5e1321fc50cf64b3abe32ca59d26e10c9
prisca onyeagoro <priscaonyeagoro@gmail.com>
2024-09-13 04:14:45 +0100

    sticky mobile header.tsx

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,10 +1,11 @@
 import ArrowRight from "@/assets/arrow-right.svg";
 import Logo from "@/assets/logosaas.png";
 import Image from "next/image";
+import MenuIcon from "@/assets/menu.svg";
 
 export const Header = () => {
   return (
-    <header>
+    <header className="sticky top-0">
       <div className="bg-black text-white text-sm flex justify-center items-center py-3">
         <div className="inline-flex gap-1 items-center">
           <p>Get started for free</p>
@@ -14,7 +15,10 @@ export const Header = () => {
 
       <div className="py-5">
         <div className="container">
-          <Image src={Logo} alt="sass logo" height={40} width={40} />
+          <div className="flex justify-between items-center">
+            <Image src={Logo} alt="sass logo" height={40} width={40} />
+            <MenuIcon className="w-5 h-5" />
+          </div>
         </div>
       </div>
     </header>
