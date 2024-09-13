44e96cff2d2221eb50d076d128ef162730050753
prisca onyeagoro <priscaonyeagoro@gmail.com>
2024-09-13 11:09:19 +0100

    header.tsx completed

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -6,7 +6,10 @@ import MenuIcon from "@/assets/menu.svg";
 export const Header = () => {
   return (
     <header className="sticky top-0">
-      <div className="bg-black text-white text-sm flex justify-center items-center py-3">
+      <div className="bg-black text-white text-sm flex justify-center items-center py-3 gap-3">
+        <p className="hidden md:block text-white/60">
+          Streamline your workflow and boost your productivity
+        </p>
         <div className="inline-flex gap-1 items-center">
           <p>Get started for free</p>
           <ArrowRight className="h-4 w-4 inline-flex justify-center items-center" />
@@ -17,7 +20,17 @@ export const Header = () => {
         <div className="container">
           <div className="flex justify-between items-center">
             <Image src={Logo} alt="sass logo" height={40} width={40} />
-            <MenuIcon className="w-5 h-5" />
+            <MenuIcon className="w-5 h-5 md:hidden" />
+            <nav className="hidden md:flex gap-6 text-black/60 items-center">
+              <a href="#">About</a>
+              <a href="#">Features</a>
+              <a href="#">Customers</a>
+              <a href="#">Updates</a>
+              <a href="#">Help</a>
+              <button className="bg-black text-white py-2 px-4 rounded-lg font-medium inline-flex align-center tracking-tight justify-center">
+                Get for free
+              </button>
+            </nav>
           </div>
         </div>
       </div>
