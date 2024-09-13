d87fb9be3610641f4531859eeec1ca409baceee3
prisca onyeagoro <priscaonyeagoro@gmail.com>
2024-09-13 13:09:24 +0100

    hero.tsx

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,24 @@
 export const Hero = () => {
-  return null;
+  return (
+    <section>
+      <div className="container">
+        <div>
+          <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-tight">
+            Version 2.0 is here
+          </div>
+          <h1 className="mt-6 text-5xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text">
+            Pathway to productivity
+          </h1>
+          <p className="mt-6">
+            Celebrate the joy of accomplishment with an app designed to track
+            your progress, motivate your efforts and celebrate your success
+          </p>
+          <div>
+            <button>Get for free</button>
+            <button>Learn more</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
