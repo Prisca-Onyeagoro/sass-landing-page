3f8fa983ec732d54b60d0ebe4f19f2dbf3455e63
prisca onyeagoro <priscaonyeagoro@gmail.com>
2024-09-14 22:42:00 +0100

    hero.tsx background radial gradient

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,6 +1,10 @@
+import ArrowIcon from "@/assets/arrow-right.svg";
+import cogImage from "@/assets/cog.png";
+import Image from "next/image";
+
 export const Hero = () => {
   return (
-    <section>
+    <section className="pt-8 pb-20 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_66%)]">
       <div className="container">
         <div>
           <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-tight">
@@ -9,14 +13,21 @@ export const Hero = () => {
           <h1 className="mt-6 text-5xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text">
             Pathway to productivity
           </h1>
-          <p className="mt-6">
+          <p className="mt-6 tracking-tight text-xl text-[#010D3E]">
             Celebrate the joy of accomplishment with an app designed to track
             your progress, motivate your efforts and celebrate your success
           </p>
-          <div>
-            <button>Get for free</button>
-            <button>Learn more</button>
+          <div className="flex gap-2 items-center mt-[30px]">
+            <button className="btn btn-primary">Get for free</button>
+            <button className="btn btn-text gap-2">
+              <span>Learn more</span>
+              <ArrowIcon className="h-5 w-5" />
+            </button>
           </div>
+        </div>
+        {/* image */}
+        <div className="mt-20">
+          <Image src={cogImage} alt={"cog image"} />
         </div>
       </div>
     </section>
