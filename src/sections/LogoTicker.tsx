38a833b536eaf1b45e7a295cf8b3ee97d324388a
prisca onyeagoro <priscaonyeagoro@gmail.com>
2024-09-29 11:16:49 +0100

    masked images

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,30 @@
+import acme from "@/assets/logo-acme.png";
+import quantum from "@/assets/logo-quantum.png";
+import echo from "@/assets/logo-echo.png";
+import celestial from "@/assets/logo-celestial.png";
+import pulse from "@/assets/logo-pulse.png";
+import apex from "@/assets/logo-apex.png";
+import Image from "next/image";
+
 export const LogoTicker = () => {
-  return null;
+  return (
+    <div className="py-8 md:py-12 bg-white">
+      <div className="container">
+        <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
+          <div className="flex gap-14 flex-none">
+            <Image src={acme} alt="acme-logo" className="logo-ticker" />
+            <Image src={quantum} alt="quantum-logo" className="logo-ticker" />
+            <Image src={echo} alt="echo-logo" className="logo-ticker" />
+            <Image
+              src={celestial}
+              alt="celestial-logo"
+              className="logo-ticker"
+            />
+            <Image src={pulse} alt="pulse-logo" className="logo-ticker" />
+            <Image src={apex} alt="apex-logo" className="logo-ticker" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
