45918450417014aa5f3f4febe26ab90a141b356a
prisca onyeagoro <priscaonyeagoro@gmail.com>
2024-10-26 17:06:08 +0100

    pricing cards --> mobile and desktop

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -1,5 +1,9 @@
+import Check from "@/assets/check.svg";
+import { twMerge } from "tailwind-merge";
+
 const pricingTiers = [
   {
+    num: 1,
     title: "Free",
     monthlyPrice: 0,
     buttonText: "Get started for free",
@@ -14,6 +18,7 @@ const pricingTiers = [
     ],
   },
   {
+    num: 2,
     title: "Pro",
     monthlyPrice: 9,
     buttonText: "Sign up now",
@@ -30,6 +35,7 @@ const pricingTiers = [
     ],
   },
   {
+    num: 3,
     title: "Business",
     monthlyPrice: 19,
     buttonText: "Sign up now",
@@ -51,5 +57,85 @@ const pricingTiers = [
 ];
 
 export const Pricing = () => {
-  return null;
+  return (
+    <section className="py-24">
+      <div className="container">
+        <div className="section-heading">
+          <h2 className="section-title">Pricing</h2>
+          <p className="section-description   mt-5">
+            Free forever. Upgrade for unlimited tasks, better security, and
+            exclusive features
+          </p>
+        </div>
+        <div className="flex flex-col gap-6 items-center mt-10 lg:flex-row lg:items-end lg:justify-center">
+          {pricingTiers.map(
+            ({
+              num,
+              title,
+              monthlyPrice,
+              buttonText,
+              popular,
+              inverse,
+              features,
+            }) => (
+              <div
+                key={1}
+                className={twMerge(
+                  "p-10 border border-[#F1F1F1] rounded-3xl shadow[0_7px_14px_#EAEAEA] max-w-xs w-full",
+                  inverse === true && "bg-black border-black text-white"
+                )}
+              >
+                <div>
+                  <div className="flex justify-between">
+                    <h3
+                      className={twMerge(
+                        "text-lg text-bold text-black/50",
+                        inverse === true && "text-white/60"
+                      )}
+                    >
+                      {title}
+                    </h3>
+
+                    {popular === true && (
+                      <div className="inline-flex text-sm px-4 rounded-xl border border-white/20">
+                        <span className="bg-[linear-gradient(to_right,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3BFFFF,#DD7DDF)] text-transparent bg-clip-text font-medium">
+                          Popular
+                        </span>
+                      </div>
+                    )}
+                  </div>
+                  <div className="flex items-baseline gap-1 mt-[30px]">
+                    <span className="text-4xl font-bold tracking-tighter leading-none">
+                      ${monthlyPrice}
+                    </span>
+                    <span className="tracking-tight font-bold text-black/50">
+                      /month
+                    </span>
+                  </div>
+                  <button
+                    className={twMerge(
+                      "btn btn-primary w-full mt-[30px]",
+                      inverse === true && "bg-white text-black"
+                    )}
+                  >
+                    {buttonText}
+                  </button>
+                  <ul className="flex flex-col gap-5 mt-8">
+                    {features.map((feature) => (
+                      <div key={1}>
+                        <li className="text-sm items-center gap-1">
+                          <Check className="h-6 w-6" />
+                          <span>{feature}</span>
+                        </li>
+                      </div>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            )
+          )}
+        </div>
+      </div>
+    </section>
+  );
 };
--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,21 +1,40 @@
 import productImage from "@/assets/product-image.png";
+import pyramid from "@/assets/pyramid.png";
+import tube from "@/assets/tube.png";
 import Image from "next/image";
 
 export const ProductShowcase = () => {
   return (
-    <section className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24">
+    <section className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 overflow-x-clip">
       <div className="container">
-        <div className="flex justify-center">
-          <div className="tag">Boost your productivity</div>
+        <div className="section-heading">
+          <div className="flex justify-center">
+            <div className="tag">Boost your productivity</div>
+          </div>
+          <h2 className="section-title  mt-5">
+            A more effective way to track progress
+          </h2>
+          <p className="section-description mt-5">
+            Effortly turn your ideas into a fully functional, responsive, Saas
+            website in just minutes with this templates
+          </p>
         </div>
-        <h2 className="text-center font-bold text-3xl tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-5">
-          A more effective way to track progress
-        </h2>
-        <p className="text-center text-[22px] leading-[30px] tracking-tight text-[#010D3E] mt-5">
-          Effortly turn your ideas into a fully functional, responsive, Saas
-          website in just minutes with this templates
-        </p>
-        <Image src={productImage} alt="productimage" className="mt-10" />
+        <div className="relative">
+          <Image src={productImage} alt="productimage" className="mt-10" />
+          <Image
+            src={pyramid}
+            alt="pyramid"
+            height={262}
+            width={262}
+            className="absolute -right-36 -top-32 hidden md:block"
+          />
+          <Image
+            src={tube}
+            alt="tube image"
+            height={248}
+            className="absolute bottom-24 -left-36 hidden md:block"
+          />
+        </div>
       </div>
     </section>
   );
