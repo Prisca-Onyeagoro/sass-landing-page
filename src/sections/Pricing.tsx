cd240cb4658cf92f428ee89e0371f95aa887797c
prisca onyeagoro <priscaonyeagoro@gmail.com>
2024-10-27 10:27:42 +0000

    testimonial section ---> mobile

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -58,7 +58,7 @@ const pricingTiers = [
 
 export const Pricing = () => {
   return (
-    <section className="py-24">
+    <section className="py-24 bg-white">
       <div className="container">
         <div className="section-heading">
           <h2 className="section-title">Pricing</h2>
@@ -81,7 +81,7 @@ export const Pricing = () => {
               <div
                 key={1}
                 className={twMerge(
-                  "p-10 border border-[#F1F1F1] rounded-3xl shadow[0_7px_14px_#EAEAEA] max-w-xs w-full",
+                  "card",
                   inverse === true && "bg-black border-black text-white"
                 )}
               >
--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -7,6 +7,7 @@ import avatar6 from "@/assets/avatar-6.png";
 import avatar7 from "@/assets/avatar-7.png";
 import avatar8 from "@/assets/avatar-8.png";
 import avatar9 from "@/assets/avatar-9.png";
+import Image from "next/image";
 
 const testimonials = [
   {
@@ -65,6 +66,45 @@ const testimonials = [
   },
 ];
 
+const firstColumn = testimonials.slice(0, 3);
+const secondColumn = testimonials.slice(3, 6);
+const thirdColumn = testimonials.slice(6, 9);
+
 export const Testimonials = () => {
-  return null;
+  return (
+    <section className="bg-white">
+      <div className="container">
+        <div className="flex justify-center">
+          <div className="tag">Testimonials</div>
+        </div>
+        <h2 className="section-title mt-5">What our users say</h2>
+        <p className="section-description mt-5">
+          From inttutive design to powerful features, our app has become an
+          essential tool to users around the world
+        </p>
+        <div className="flex flex-col mt-10 gap-6 [mask-image:linear-gradient(to_bottom,transparent,black_25%,black_75%,transparent)]">
+          {firstColumn.map(({ text, imageSrc, name, username }) => (
+            <div key={1} className="card">
+              <div>{text}</div>
+              <div className="flex items-center gap-2 mt-5">
+                <Image
+                  src={imageSrc}
+                  alt={name}
+                  width={40}
+                  height={40}
+                  className="h-10 w-10 rounded-full"
+                />
+                <div className="flex flex-col">
+                  <div className="font-medium leading-5 tracking-tight">
+                    {name}
+                  </div>
+                  <div className="leading-5 tracking-tight">{username}</div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
